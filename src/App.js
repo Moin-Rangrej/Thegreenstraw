d9b3aab6f213610d146bca7c40950debd579bd6f
Moin Rangrej <rangrejmoin1234@gmail.com>
2023-04-29 19:59:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,23 +9,26 @@ import All from './pages/gallery/All';
 import Cafes from './pages/gallery/Cafes';
 import Cowork from './pages/gallery/Cowork';
 import Store from './pages/gallery/Store';
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import 'lightbox2/dist/css/lightbox.css'
+import 'lightbox2/dist/js/lightbox-plus-jquery'
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/aboutus' element={<Aboutus />}/>
-        <Route path='/services' element={<Services />}/>
-        <Route path='/gallery' element={<Gallery />}>
-            <Route path='all' element={<All />}/>
-            <Route path='cafes' element={<Cafes />}/>
-            <Route path='cowork' element={<Cowork />}/>
-            <Route path='store' element={<Store />}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/aboutus' element={<Aboutus />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/gallery/' element={<Gallery />}>
+          
+          <Route path='all' element={<All />} />
+          <Route path='cafes' element={<Cafes />} />
+          <Route path='cowork' element={<Cowork />} />
+          <Route path='store' element={<Store />} />
         </Route>
         <Route path='/contactus/' element={<Contactus />} />
       </Routes>
